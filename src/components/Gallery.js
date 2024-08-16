7793013a6ea4932cc7a0883955de26e58a5604af
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2024-08-16 21:45:17 +0530

    new

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,11 +5,17 @@ import '../Gallery.css'; // Assuming you save the CSS above in this file
 export default function Gallery(props) {
     const images = [
         { src: require("../image/image6.jpg"), text: "Helmet Detection" },
+
         { src: require("../image/image5.jpg"), text: "Detection of Cotton Leaves Diseases" },
+
         { src: require("../image/image4.jpg"), text: "BasketBall" },
+
         { src: require("../image/image3.jpg"), text: "BasketBall" },
+
         { src: require("../image/image1.jpg"), text: "Workshop" },
+
         { src: require("../image/image2.jpg"), text: "BasketBall" },
+        
         // Add more images with their descriptions here
     ];
 
